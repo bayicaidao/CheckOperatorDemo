6e91262d93d71fe454ec45fcf1a92cff647e4953
ZhangMeng <mayizhao@ZhangMengdeMac-mini.local>
2013-09-29 09:43:54 +0800

    Initial Commit

---
+++ CheckOperatorDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CheckOperatorDemo
+//
+//  Created by ZhangMeng on 13-9-29.
+//  Copyright (c) 2013年 zhangM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CheckOperatorDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CheckOperatorDemo
+//
+//  Created by ZhangMeng on 13-9-29.
+//  Copyright (c) 2013年 zhangM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
